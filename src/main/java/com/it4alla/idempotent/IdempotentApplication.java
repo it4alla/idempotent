72363c419e180a4a9b20651bfddbc7b0e2b1acb0
IT云清 <1186355422@qq.com>
2020-04-15 12:52:27 +0800

    init:init the project

---
+++ src/main/java/com/it4alla/idempotent/IdempotentApplication.java
@@ -0,0 +1,16 @@
+package com.it4alla.idempotent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author IT云清
+ */
+@SpringBootApplication
+public class IdempotentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdempotentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/it4alla/idempotent/IdempotentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it4alla.idempotent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
